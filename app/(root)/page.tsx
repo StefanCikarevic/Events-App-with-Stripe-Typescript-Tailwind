470c0938ec08ac719e75d1c26b998d7390921f9d
StefanCikarevic <stefan.cikarevic@prodyna.com>
2023-12-21 12:14:19 +0100

    Init App

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Evently</h1>
+      <Button variant={"destructive"} className={"px-10"}>
+        Hello
+      </Button>
+    </main>
+  );
+}
