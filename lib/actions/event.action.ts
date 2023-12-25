6df321a75f26e1d9f39dd5bf84fd91df6a3153b7
StefanCikarevic <stefan.cikarevic@prodyna.com>
2023-12-25 13:23:14 +0100

    Event details page

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "@/lib/database";
 import User from "@/lib/database/models/user.model";
 import Event from "@/lib/database/models/event.model";
 import { handleError } from "@/lib/utils";
+import Category from "@/lib/database/models/category.model";
 export const createEvent = async ({
   event,
   userId,
@@ -26,3 +27,27 @@ export const createEvent = async ({
     console.log(err);
   }
 };
+
+const populateEvent = (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
+
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+}
